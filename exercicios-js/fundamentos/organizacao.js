4fa1feababe4e8788bdda0b8c2cc5dd2b4e598c6
Gabriel_ferlima <Gabriel_fernandes1995@hotmail.com>
2022-06-15 11:04:39 -0300

    aula

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,17 @@
+console.log("Setenção de código")
+
+const avaliacao = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao * avaliacao2 
+const media = total 
+
+console.log(media)
+
+const raio = 5.6 
+const area = Math.PI * Math.pow(raio, 2)
+console.log(area)
+
+const escola = 'Cod3r'
+
+console.log(escola.charAt(4))
